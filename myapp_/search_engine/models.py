ab33542023ed6737dc4e0fc025aedc5169e420b5
wlisonoao <wlisonouo@iir.csie.ncku.edu.tw>
2024-10-18 14:26:00 +0000

    1018_v2

--- myapp_/search_engine/models.py
+++ myapp_/search_engine/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Uploadxml(models.Model):
     file = models.FileField(upload_to='./search_engine/data')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Word(models.Model):
+    
+
+class article(models.Model):
+    file = models.CharField(max_length=200)
+    word = models.ForeignKey(Word, on_delete=models.CASCADE, , related_name='terms')
